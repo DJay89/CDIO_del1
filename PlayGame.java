baf3ddb9588553914d3bbe9da068ab2e51bcafcd
Freya Helstrup <freyagh@me.com>
2016-10-03 13:08:28 +0200

    Update PlayGame.java

--- PlayGame.java
+++ PlayGame.java
@@ -1,22 +1,23 @@
-package dicegame;
+package del1;
+
+import desktop_resources.GUI;
 
 public class PlayGame {
 
 	public static void main(String[] args) {
+		
 		Player p1 = new Player(1,0,true);
 		Player p2 = new Player(2,0,false);
 		Player currentPlayer = new Player(0,0,false);
 
-		Dice d1 = new Dice();
-		Dice d2 = new Dice();
+		Dice dice1 = new Dice();
+		Dice dice2 = new Dice();
 		
-		int prevRoll1 = 0;
-		int prevRoll2 = 0;
 		
-		while (p1.score < 40 && p2.score < 40)
+		while (p1.getScore() < 40 && p2.getScore() < 40)
 		{
 		//define who's turn it is
-			if (p1.isTurn == true){
+			if (p1.getIsTurn() == true){
 				currentPlayer = p1;
 			}
 			else {
@@ -25,20 +26,23 @@ public class PlayGame {
 			
 		//print who's turn it is
 			int nextPlayerId;
-			if (p1.isTurn == true){
-				nextPlayerId = p1.id;
+			if (p1.getIsTurn() == true){
+				nextPlayerId = p1.getId();
 			}
 			else {
-				nextPlayerId = p2.id;
+				nextPlayerId = p2.getId();
 			}
-				
-			System.out.println("Det er spiller " + nextPlayerId + "'s tur.");
-			
+
+			GUI.getUserButtonPressed("Det er spiller " + nextPlayerId + "'s tur.", "Slå terninger");
+//			System.out.println("Det er spiller " + nextPlayerId + "'s tur.");
+	
 		//roll the dice
-			int roll1 = d1.roll(); 
-			int roll2 = d2.roll();
+			int roll1 = dice1.roll(); 
+			int roll2 = dice2.roll();
 			
-			System.out.println(roll1 + " " + roll2);
+			GUI.setDice(roll1, roll2);
+
+//			System.out.println(roll1 + " " + roll2);
 			
 		//A player shall lose his score if the 
 		//result of the dice roll is a pair of ones
@@ -48,7 +52,8 @@ public class PlayGame {
 		//A player shall win the game, if the player 
 		//throws a pair of 6’s twice, regardless from 
 		//an extra turn or his previous turn.
-			else if(roll1 == 6 && roll2 == 6 && prevRoll1 == 6 && prevRoll2 == 6){
+			else if(roll1 == 6 && roll2 == 6 
+					&& currentPlayer.getPrevTurn1() == 6 && currentPlayer.getPrevTurn2() == 6){
 				currentPlayer.setScore(40);
 			}
 		//Add dice values to current score
@@ -60,34 +65,38 @@ public class PlayGame {
 		//the result of the dice throw is a pair.
 			if (roll1 == roll2){ 
 				if (currentPlayer == p1){
-					p1.isTurn = true;
+					p1.setIsTurn(true);
 				}
 				else {
-					p1.isTurn = false;
+					p1.setIsTurn(false);
 				}
 			}
 			else {
 				if (currentPlayer == p1){
-					p1.isTurn = false;
+					p1.setIsTurn(false);
 				}
 				else {
-					p1.isTurn = true;
+					p1.setIsTurn(true);
 				}
 			}
 		
 		//Print the score for both players
-			System.out.println(p1 + "\t" + p2);
-			System.out.println("-----------------------------------------------");
+			GUI.showMessage(p1 + "\t" + p2);
+//			System.out.println(p1 + "\t" + p2);
+//			System.out.println("-----------------------------------------------");
 		
 		//Save the rolls in prevRoll1 and prevRoll2
-			prevRoll1 = roll1;
-			prevRoll2 = roll2;
-		}
-		
+			if (currentPlayer == p1){
+				p1.setPrevTurn(roll1, roll2);
+			}
+			else {
+				p2.setPrevTurn(roll1, roll2);
+			}
+		}	
 		//Define the winner
 		Player winner = new Player(0,0,false);
 		
-		if (p1.score>=40){
+		if (p1.getScore() >= 40){
 			winner = p1;
 		}
 		else {
@@ -95,6 +104,10 @@ public class PlayGame {
 		}
 		
 		//Print the winner
-		System.out.println("Vinderen er: Spiller " + winner.id + "!");	
+		GUI.showMessage("Vinderen er: Spiller " + winner.getId() + "!");
+		
+		GUI.close();
+		
+//		System.out.println("Vinderen er: Spiller " + winner.id + "!");	
 	}
 }
