b2589d40ea0073e1d0be1b80454ed431696536c8
Freya Helstrup <freyagh@me.com>
2016-10-03 13:07:50 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,9 +1,9 @@
-package dicegame;
+package del1;
 
 public class Player {
 
-	int id, score;
-	boolean isTurn;
+	private int id, score, prevTurn1, prevTurn2;
+	private boolean isTurn;
 	
 	public Player (int id, int score, boolean isTurn)
 	{
@@ -11,23 +11,43 @@ public class Player {
 		this.score = score;
 		this.isTurn = isTurn;
 	}
-	
-	public void setScore(int score){
-		this.score = score;
-	}
 
 	public void addScore(int point){
 		this.score += point;
 	}
 
-	public String getScore(){
-		return "" + score;
+	public void setScore(int score){
+		this.score = score;
 	}
 
-	public String getId(){
-		return "" + id;
+	public void setIsTurn(boolean isTurn){
+		this.isTurn = isTurn;
 	}
 
+	public void setPrevTurn(int prevTurn1, int prevTurn2){
+		this.prevTurn1 = prevTurn1;
+		this.prevTurn2 = prevTurn2;
+	}
+	
+	public int getId(){
+		return id;
+	}
+
+	public int getScore(){
+		return score;
+	}
+	public boolean getIsTurn(){
+		return isTurn;
+	}
+
+	public int getPrevTurn1(){
+		return prevTurn1;
+	}
+
+	public int getPrevTurn2(){
+		return prevTurn2;
+	}
+	
 	public String toString(){
 		return "Spiller " + id + " har " + score + " point.";
 	}
